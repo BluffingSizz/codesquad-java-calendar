7fd1fa9fecc4b097fc4a12db18cc3506ad63352b
User <User@mr02-03>
2021-04-02 16:50:54 +0900

    21 04 02 inint

--- src/hunux/calendar/Calendar.java
+++ src/hunux/calendar/Calendar.java
@@ -9,16 +9,39 @@ public class Calendar {
 		return max_date[month-1];
 	}
 	
-	public void printCalendar(int year,int month) {
+	public void printCalendar(int year,int month,String first) {
 		if(year%4==0) {
 			max_date[1] = 29;
+		}
+		int a =0;
+		switch (first) {
+		case "일" : a=0;
+		break;
+		case "월" : a=1;
+		break;
+		case "화" : a=2;
+		break;
+		case "수" : a=3;
+		break;
+		case "목" : a=4;
+		break;
+		case "금" : a=5;
+		break;
+		default : a=-1;
+		break;
 		}
 		System.out.printf("   <<%4d년%3d월>> \n",year,month);
 		 System.out.println(" SU MO TU WE TH FR SA");
 		 System.out.println("---------------------");
+		 if(a==0 || a==-1) {
+		 }else {
+			 for(int j=0; j<a; j++) {
+				 System.out.print("   ");
+			 }
+		 }
 		 for(int i=1; i<=maxDaysOfMonth(month); i++) {
 			 System.out.printf("%3d",i);
-			 if(i%7 == 0) {
+			 if((i+a)%7 == 0) {
 				 System.out.println();
 			 }
 		 }
@@ -29,7 +52,9 @@ public class Calendar {
 		// 숫자를 입력받아 해당하는 달을 출력하는 프로그램
 		int year=2021;
 		int month;
+		String first;
 		Scanner scan = new Scanner(System.in);
+		System.out.println(java.util.Calendar.);
 		Calendar cal = new Calendar();
 		String prompt = "Cal> ";
 		while(true) {
@@ -39,12 +64,15 @@ public class Calendar {
 			System.out.println("달을 입력하세요");
 			System.out.print(prompt);
 			month = scan.nextInt();
+			System.out.println("1일의 요일을 입력하세요(월,화,수,목,금,일");
+			System.out.print(prompt);
+			first = scan.next();
 			if(month ==-1) {
 				break;
 			}else if(month >12 || month<1){
 				continue;
 			}else {
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, first);
 			System.out.println();
 			}
 		}
